ba5708958e1eba1a1e245aa4ab27ae02ea09a15f
Michael Holtzscher <thenerdproject1@gmail.com>
2018-05-20 08:35:39 -0500

    code cleanup

--- syllapy/__init__.py
+++ syllapy/__init__.py
@@ -12,18 +12,18 @@ def count(word):
 
 
 def _syllables(token):
-    syll_count = 0
+    syllable_count = 0
     vowels = 'aeiouy'
     word = token.lower().strip(".:;?!")
     if word[0] in vowels:
-        syll_count += 1
+        syllable_count += 1
     for index in range(1, len(word)):
         if word[index] in vowels and word[index - 1] not in vowels:
-            syll_count += 1
+            syllable_count += 1
     if word.endswith('e'):
-        syll_count -= 1
+        syllable_count -= 1
     if word.endswith('le') and len(word) > 2 and word[-3] not in vowels:
-        syll_count += 1
-    if syll_count == 0:
-        syll_count += 1
-    return syll_count
+        syllable_count += 1
+    if syllable_count == 0:
+        syllable_count += 1
+    return syllable_count
