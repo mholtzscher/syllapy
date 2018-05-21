0a7818af8e626d600dab8641f29b3e3e987da882
Michael Holtzscher <thenerdproject1@gmail.com>
2018-05-21 12:45:09 -0500

    remove spacy from setup

--- setup.py
+++ setup.py
@@ -7,36 +7,35 @@ from setuptools import setup, find_packages
 
 
 def setup_package():
-    package_name = 'syllapy'
-    root = Path(__file__).parent.resolve()
+	package_name = 'syllapy'
+	root = Path(__file__).parent.resolve()
 
-    # Read in package meta from about.py
-    about_path = root / package_name / 'about.py'
-    with about_path.open('r', encoding='utf8') as f:
-        about = {}
-        exec(f.read(), about)
+	# Read in package meta from about.py
+	about_path = root / package_name / 'about.py'
+	with about_path.open('r', encoding='utf8') as f:
+		about = {}
+		exec(f.read(), about)
 
-    # Get readme
-    readme_path = root / 'README.rst'
-    with readme_path.open('r', encoding='utf8') as f:
-        readme = f.read()
+	# Get readme
+	readme_path = root / 'README.rst'
+	with readme_path.open('r', encoding='utf8') as f:
+		readme = f.read()
 
-    setup(
-        name=package_name,
-        description=about['__summary__'],
-        long_description=readme,
-        author=about['__author__'],
-        author_email=about['__email__'],
-        url=about['__uri__'],
-        version=about['__version__'],
-        license=about['__license__'],
-        packages=find_packages(),
-        install_requires=[
-            'spacy>=2.0.0,<3.0.0',
-        ],
-        zip_safe=False,
-    )
+	setup(
+		name=package_name,
+		description=about['__summary__'],
+		long_description=readme,
+		author=about['__author__'],
+		author_email=about['__email__'],
+		url=about['__uri__'],
+		version=about['__version__'],
+		license=about['__license__'],
+		packages=find_packages(),
+		install_requires=[
+		],
+		zip_safe=False,
+	)
 
 
 if __name__ == '__main__':
-    setup_package()
+	setup_package()
