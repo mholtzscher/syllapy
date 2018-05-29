02514fc85000c002c7b4025bbcddb705c052626e
Michael Holtzscher <thenerdproject1@gmail.com>
2018-05-29 16:44:56 -0500

    Remove history from long_description

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -33,7 +30,7 @@ setup(
     description="Calculate syllables for English words.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='syllapy',
     name='syllapy',
