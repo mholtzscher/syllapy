aac387ed9e97b59ee741c774099ba40b30f5ed0d
Michael Holtzscher <mholtz@protonmail.com>
2018-06-10 01:39:53 +0000

    Bump version: 0.2.1 → 0.3.0

--- syllapy/__init__.py
+++ syllapy/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Michael Holtzscher"""
 __email__ = "mholtz@protonmail.com"
-__version__ = "0.2.1"
+__version__ = "__version__ = '0.3.0'"
 
 import logging
 from string import punctuation
