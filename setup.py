4c80449150320364476ed77ab876d1bd3e418604
mholtzscher <mholtzscher@fanthreesixty.com>
2018-09-12 12:27:12 -0500

    fixed tests

--- setup.py
+++ setup.py
@@ -8,32 +8,7 @@ import sys
 
 from codecs import open
 
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
+from setuptools import setup, find_packages
 
 # 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
@@ -75,5 +50,4 @@ setup(
     url='https://github.com/mholtzscher/syllapy',
     version='0.4.0',
     zip_safe=False,
-    cmdclass={'test': PyTest},
 )
