7e0e2a84179a8868d05121f3b3dd139b17a23637
Michael Holtzscher <mholtz@protonmail.com>
2018-11-01 19:26:17 -0500

    Revert changes to fix pylint and mypy erros

--- syllapy/__init__.py
+++ syllapy/__init__.py
@@ -19,7 +19,13 @@ NUMBERS = re.compile(r"\d")
 WORD_DICT = load_dict()
 
 
-def count(word: float) -> int:
+def count(word: str) -> int:
+    """Returns number of syllables in a word.
+    If the word is None, not a string, contains invalid chars, or empty then returns 0.
+    :rtype: int
+    :param word: the word to count syllables for
+    :return: the number of syllables in the word
+    """
     try:
         word = word.strip().lower().strip(punctuation)
         if not word:
