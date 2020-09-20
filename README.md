eb870f5fa3a077bbe3bba189254bcc64186bcc0d
Michael Holtzscher <mholtz@protonmail.com>
2020-09-20 15:14:24 -0500

    remove azure pipelines for github actions (#89)

--- README.md
+++ README.md
@@ -3,10 +3,7 @@ SyllaPy
 
 <p>
   <a href="https://badge.fury.io/py/syllapy"><img src="https://badge.fury.io/py/syllapy.svg" alt="PyPI version"></a>
-  <a href="https://mholtzscher.visualstudio.com/syllapy/_build"><img src="https://mholtzscher.visualstudio.com/syllapy/_apis/build/status/mholtzscher.syllapy?branchName=master" alt="Azure Pipelines"></a>
   <a href="https://github.com/mholtzscher/syllapy"><img src="https://img.shields.io/badge/code%20style-black-000000.svg" alt="Black"></a>
-  <a href="https://codecov.io/gh/mholtzscher/syllapy">
-  <img src="https://codecov.io/gh/mholtzscher/syllapy/branch/master/graph/badge.svg" alt="Codecov"/></a>
 </p>
 
 Calculate syllable counts for English words.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="syllapy",
+    version="0.7.1",
+    url="https://github.com/mholtzscher/syllapy",
+    license="MIT",
+    author="Michael Holtzscher",
+    author_email="michael.holtzscher@gmail.com",
+    description="Calculate syllable counts for English words.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["syllables", "nlp"],
+    python_requires=">=3.6, <4",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
